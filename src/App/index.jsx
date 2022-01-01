a235f1307ea0756536bc2427a11f79e5ca8dc890
Emmanuel Ikem <emmanuel.ikem@botosoft.net>
2022-01-01 09:39:01 +0100

    App bug Modifications

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 import '@github/g-emoji-element';
 import {Routes, Route} from 'react-router-dom';
 import Layout from '../Layout';
-import Trending from '../pages/Trending';
+// import Trending from '../pages/Trending';
 import {Developers} from '../pages/Developer';
-
 
 function App() {
 	return (
@@ -14,6 +13,7 @@ function App() {
 				<Route path='/' element={<Layout />}>
 					<Route path='trending' element={<Layout />} />
 					<Route path='/developers' element={<Developers />} />
+					<Route path='*' element={<h1>No Data Found</h1>} />
 				</Route>
 			</Routes>
 		</>
